255e7b72bcecb7632706ea7d6feb185a804b2044
Your Name <saad.khan955@gmail.com>
2023-05-21 02:55:49 +0530

    fix

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
 
 // var url = 'https://cors-anywhere.herokuapp.com/http://www.filltext.com/?rows=32&id={number|1000}&firstName={firstName}&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32}';
 
-var url = 'https://cors.bridged.cc/http://www.filltext.com/?rows=32&id={number|1000}&firstName={firstName}&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32}';
+var url = 'https://cors.bridged.cc/http://www.filltext.com/?rows=32&id=%7Bnumber%7C1000%7D&firstName=%7BfirstName%7D&lastName=%7BlastName%7D&email=%7Bemail%7D&phone=%7Bphone%7C(xxx)xxx-xx-xx%7D&address=%7BaddressObject%7D&description=%7Blorem%7C32%7D';
 
 
   // prevent cors error due to http request
